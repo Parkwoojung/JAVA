6cf73ecdfdc15a701c7c4eda83b84baa0a8df251
WOOJUNG <lowell0322@naver.com>
2022-09-14 21:35:26 +0900

    220914 백준 1008

---
+++ src/TEST/Java_0913_1008.java
@@ -0,0 +1,17 @@
+package TEST;
+
+import java.util.Scanner;
+
+public class Java_0913_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextInt();
+        float b = sc.nextInt();
+
+        float c = a/b;
+
+        System.out.println(c);
+
+    }
+}
