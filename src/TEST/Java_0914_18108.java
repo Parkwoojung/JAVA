a6d667a4354695bbdc718dfd8556a464890eaf6e
WOOJUNG <lowell0322@naver.com>
2022-09-22 19:43:39 +0900

    220922 백준 18108

---
+++ src/TEST/Java_0914_18108.java
@@ -0,0 +1,15 @@
+package TEST;
+
+import java.util.Scanner;
+
+public class Java_0914_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.println(a-543);
+
+
+
+    }
+}
