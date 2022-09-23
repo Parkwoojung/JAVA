62c14239ecdee902bf8f6862fd559678b4c01767
WOOJUNG <lowell0322@naver.com>
2022-09-23 22:56:51 +0900

    220923 백준 25083

---
+++ src/TEST/Java_0920_25083.java
@@ -0,0 +1,12 @@
+package TEST;
+
+public class Java_0920_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                             "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
